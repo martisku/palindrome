3dcaaec699484e820d35ce30adfb8e5376a3b6b0
Martin Skurtveit <martin.skurtveit@gmail.com>
2021-10-28 03:07:05 +0200

    Update endpoints

--- src/main/java/no/sku/controllers/PersonController.java
+++ src/main/java/no/sku/controllers/PersonController.java
@@ -1,11 +1,15 @@
 package no.sku.controllers;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import no.sku.person.Person;
 import no.sku.person.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -22,24 +26,40 @@ public class PersonController {
     }
 
     @GetMapping
-    public List<Person> allPersons() {
-        return PersonRepository.fetchAll();
+    public ResponseEntity<List<Person>> allPersons() {
+        List<Person> allPersons = PersonRepository.fetchAll();
+        return ResponseEntity
+                .created(URI.create(String.format("/person/%s", allPersons.get(0).getFirstName())))
+                .body(allPersons);
     }
 
     @GetMapping("/{id}")
-    public Person findById(@PathVariable("id") UUID id) {
-        return PersonRepository.fetchPersonById(id);
+    public ResponseEntity<Person> findById(@PathVariable("id") UUID id) {
+        Person person = PersonRepository.fetchPersonById(id);
+        return ResponseEntity
+                .created(URI.create(String.format("/person/%s", person.getFirstName())))
+                .body(person);
     }
 
-    @PostMapping("/{id}")
-    public void createPerson(@RequestBody Person person) {
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
         Person newPerson = new Person(person.getFirstName(), person.getLastName());
         PersonRepository.save(newPerson);
+        return ResponseEntity
+                .created(URI.create(String.format("/person/%s", person.getFirstName())))
+                .body(newPerson);
     }
 
     @PutMapping("/{id}")
-    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person person) {
+    public ResponseEntity<Person> updatePerson(@PathVariable("id") UUID id, @RequestBody Person person) {
+        //todo: return person after update
         PersonRepository.update(id, person.getFirstName(), person.getLastName());
+        return ResponseEntity
+                .created(URI.create(String.format("/person/%s", person.getFirstName())))
+                .body(person);
     }
 
     @DeleteMapping("/{id}")
@@ -50,7 +70,6 @@ public class PersonController {
     @GetMapping("/{id}/palindrome")
     public String checkPalindrome(@PathVariable("id") UUID id) {
         Person person = PersonRepository.fetchPersonById(id);
-
         assert person != null;
         if(isPalindrome(person.getFirstName())) return person.getFirstName() + " er et palindrom!";
         if(isPalindrome(person.getLastName())) return person.getLastName() + " er et palindrom!";
